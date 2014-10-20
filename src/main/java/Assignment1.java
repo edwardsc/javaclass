80b42a1c5ce701a9d417604a81c1d0dad88e321b
Charles Edwards <edwards.charles317@gmail.com>
2014-10-20 19:50:56 -0400

    started intellij project in repo directory. added main class and added assignment1

---
+++ src/main/java/Assignment1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by EdwardsMAC on 10/20/14.
+ */
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.print("Hello World, from " + Assignment1.class.getCanonicalName());
+        
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by EdwardsMAC on 10/20/14.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.print("Hello World!");
+    }
+}
