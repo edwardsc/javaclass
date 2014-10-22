3e240b58e5c4c409d09294c1fbfee2c5380047e0
Benjamin Edwards <edwards.benw@gmail.com>
2014-10-22 00:04:24 -0400

    added the driver and logic for switching different commands in main. only distance converter is implemented right now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by EdwardsMAC on 10/20/14.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // write your code here
-        System.out.print("Hello World!");
+        System.out.println("Welcome to the Assignment Driver!");
+        System.out.println("Enter option number: ");
+        System.out.println("1 for distance converter");
+        System.out.println("2 for foo");
+        System.out.println("3 for bar");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String option = bufferedReader.readLine();
+
+        switch (option){
+            case "1":
+                DistanceFormulaConverter.calculateFeetPerMeter();
+                DistanceFormulaConverter.calculateMetersPerFoott();
+                break;
+            case "2":
+                System.out.print("Under construction");
+                break;
+            case "3":
+                System.out.print("Under construction");
+                break;
+            default:
+                System.out.print("Invalid command");
+                System.exit(-1);
+        }
+
     }
 }
